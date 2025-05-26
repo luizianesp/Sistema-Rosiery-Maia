72b1dcaf4727079463e60138773fce00fd3d0546
Boris Oliveira <92190379+kzoriz@users.noreply.github.com>
2025-05-26 18:50:35 -0300

    Delete backend directory

--- backend/core/admin.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-from .models import AreaPesquisa, Projeto, Publicacao, Orientacao, MensagemContato
-
-admin.site.register(AreaPesquisa)
-admin.site.register(Projeto)
-admin.site.register(Publicacao)
-admin.site.register(Orientacao)
-admin.site.register(MensagemContato)
-
--- backend/core/forms.py
+++
@@ -1,7 +0,0 @@
-from django import forms
-from .models import MensagemContato
-
-class ContatoForm(forms.ModelForm):
-    class Meta:
-        model = MensagemContato
-        fields = ['nome', 'email', 'assunto', 'mensagem']
